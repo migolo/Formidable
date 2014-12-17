5b484b3fa3c83838200a34300fa6402864a7edf0
Miguel González <migolo10@gmail.com>
2014-12-17 00:37:39 -0600

    Create DateTimeField.php

---
+++ Fields/DateTimeField.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Gregwar\Formidable\Fields;
+
+/**
+ * DateTime field
+ *
+ * @author Grégoire Passault <g.passault@gmail.com>
+ */
+class DateTimeField extends DateField
+{
+
+    /**
+     * Field type
+     */
+    protected $type = 'DateTime';    
+
+    
+}
